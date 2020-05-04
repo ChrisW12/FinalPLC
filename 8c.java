d129c6f41368eb46268c736595a9c1504c16749d
ChrisW12 <47219543+ChrisW12@users.noreply.github.com>
2020-05-04 15:19:43 -0400

    Create 8c.java

---
+++ 8c.java
@@ -0,0 +1,25 @@
+public class ProblemEight{
+
+     public static void main(String []args){
+        int j = -3;
+
+for ( int i = 0; i < 3; i ++) {
+	if ( j + 2 == 2 || j + 2 == 3); {
+		j --;
+	}   
+	
+    if ( j + 2 == 0) {
+		j += 2;
+    }    
+	
+		
+	else j = 0;
+
+
+
+
+if (j > 0) 
+Break;
+
+j = 3 - i;
+		}
