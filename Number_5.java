da65d868fec6a95687e695932e55402a61040179
ChrisW12 <47219543+ChrisW12@users.noreply.github.com>
2020-05-04 13:04:41 -0400

    Create Number_5.java

---
+++ Number_5.java
@@ -0,0 +1,19 @@
+ public static void main(String []args){
+        
+        boolean flag = false;
+        
+        for (int i = 1; i <= n; i++) {
+            
+            int Counter = 0;
+            for (int j = 1; j <= n; j++) { 
+                if (x[i][j] != 0) 
+                    Counter++;
+            }
+            
+            if (flag == false && Counter == n) {
+            System.out.println("First all zero row is: ", i);
+            flag = true;
+        ;
+        }
+     }
+}
