c68f3885cd08dbb5e1cbd5b1638e40013f231138
ChrisW12 <47219543+ChrisW12@users.noreply.github.com>
2020-05-04 15:30:35 -0400

    Update Number_6_Nested.java

--- Number_6_Nested.java
+++ Number_6_Nested.java
@@ -1,81 +1,74 @@
 
+public class Number_6 {
 
-public class Number6Pt2 {
 
-
-public static void main(String[] args) {
-
-	
-	int max=0,min=0,mid=0;
-
-	int a = 1;
-	int b = 2;
-	int c = 3;
+	public static void main(String[] args) {
   
+		int x = 5;
+		int y = 2;
+		int z = 3;
 	
-	long startTime = System.nanoTime();
-	
-	if(a>b) {
-		if(a>c) {
-			if(b>c) {
-				max=a;
-				min=c;
-				mid=b; 
-				}
+		int max = 0;
+		int min = 0;
+		int mid = 0;
+		
+		long startTime = System.nanoTime();
+
+
+			if(x>y && x>z && y>z) {
 			
-			else
-			{
-				max=a;
-				min=b;
-				mid=c;
+				max = x;
+				min = z;
+				mid = y;
 			}
-		}
-		else
-		{
-			max=c;
-			min=b;
-			mid=a;
-  
-  
-		}
-  
-	}
-	else
-	{
-		if(b<c)
-		{
-			max=c;
-			min=a;
-			mid=b;
+			else if(x>y && x>z && y<z) {
+			
+				max = x;
+				min = y;
+				mid = z;
+			}
+			else if(y>z && y>x && x>z) {
 
-		}	
-		else
-		{
-			if(a<c)
-			{
-				max=b;
-				min=a;
-				mid=c;  
+				max = y;
+				min = z;
+				mid = x;
 			}
-			else
-			{
-				max=b;
-				min=c;
-				mid=a;
+			else if(y>z && y>x && z>x) {
+
+			max = y;
+			min = x;
+			mid = z;
+			
 			}
-		}
+			else if(z>x && z>y && x>y) {
+
+				max = z;
+				min = y;
+				mid = x;
   
+			}
+			else {
+
+				max = z;
+				min = x;
+				mid = y;
+			}
+
+			long endTime = System.nanoTime();
+			
+			long timeElapsed = endTime - startTime;
+			
+			
+			System.out.println("Max is "+ max);
+
+			System.out.println("Min is "+ min);
+
+			System.out.println("Mid is "+ mid);
+			
+			System.out.println("Execution time in nanoseconds was: " +  timeElapsed);
+
+			
+			
 	}
   
-	long endTime = System.nanoTime();
-	
-	long timeElapsed = endTime - startTime;
-	
-	System.out.println("Max is "+ max);
-	System.out.println("Min is "+ min);
-	System.out.println("Mid is "+ mid);
-	
-	System.out.println("Execution time in nanoseconds was: " +  timeElapsed);
-	}
-  
-	}
+}
